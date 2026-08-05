3d71e72a150d81685908ff1d361c4ff0bf136fea
Lucas Lins <lucasosousa231202@gmail.com>
2026-08-05 12:25:12 -0300

    ainda tentando arrumar cors

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "https://mini-rede-social.vercel.app/",
+    origin: "https://mini-rede-social.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
